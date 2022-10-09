c71609f44da1fec30580df1564dff12e52f59444
mermer vivo <mincoln419@naver.com>
2022-10-09 09:12:11 +0900

    kotlin scope

---
+++ kotlin_exercise/src/main/kotlin/ScopeEx.kt
@@ -0,0 +1,46 @@
+fun main() {
+
+    val str: String? = "안녕"
+
+
+    var result = str?.let {
+        println(it)
+
+        val abc: String? = "abc"
+        abc?.let {
+            val def : String? = "def"
+            def?.let{
+                println("abcdef가 null이 아님")
+            }
+        }
+
+        1234
+    }
+    // depth가 많아질 때는 if/else로 하는게 가독성 측면에서 좋아보임
+
+    println(result)
+
+
+    val config = DatabaseClient()
+    config.url = "localhost:3306"
+    config.username = "mysql"
+    config.password = "1234"
+    val connected = config.connect()
+
+    println(connected)
+
+}
+
+class DatabaseClient {
+    var url: String? = null
+    var username: String? = null
+    var password: String? = null
+
+    //DB에 접속하고 Boolean 결과를 반환
+    fun connect(): Boolean {
+        println("DB 접속 중...")
+        Thread.sleep(1000)
+        println("DB 접속 완료")
+        return true
+    }
+}
