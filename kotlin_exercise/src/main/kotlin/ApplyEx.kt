ff01246018c44ef82a4006112a6813abe22c316f
mermer vivo <mincoln419@naver.com>
2022-10-10 12:21:39 +0900

    scope apply

---
+++ kotlin_exercise/src/main/kotlin/ApplyEx.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val client: DatabaseClient = DatabaseClient().apply {
+        url = "localhost:3306"
+        username = "mysql"
+        password = "1234"
+    }
+
+//    var connected = client.connect();
+//    println(connected)
+
+    client.connect().run { println(this) }
+}
