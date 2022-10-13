0a1f767f37c2831dc9788eb6aa5f836ee8378872
mermer vivo <mincoln419@naver.com>
2022-10-14 07:35:56 +0900

    kotlin springboot initialize

---
+++ springboot/springboot-kotlin/src/test/kotlin/com/mermer/springbootkotlin/SpringbootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mermer.springbootkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
